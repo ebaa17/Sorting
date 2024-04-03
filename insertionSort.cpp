386213ddf778e984ceb52240a9b0747a7493ba97
ebaa17 <109791096+ebaa17@users.noreply.github.com>
2024-04-03 02:51:15 +0200

    Update insertionSort.cpp

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -1,13 +1,6 @@
 #include <bits/stdc++.h>
 
-#define fast ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
-#define el '\n'
-#define ll  long long
-#define ld  long double
-#define all(x) x.begin(), x.end()
-#define yes "YES\n"
-#define no "NO\n"
-#define test int test;cin>>test;while(test--)
+
 using namespace std;
 
 
@@ -22,7 +15,6 @@ void insertionSort(int arr[], int n) {
 }
 
 int main() {
-    fast
     int n;
     cin >> n;
     int a[n];
@@ -32,5 +24,4 @@ int main() {
     insertionSort(a, n);
     for (int i: a) cout << i << ' ';
 }
-
 
