c809afe5f81a114099104671c305f020843cca95
ebaa17 <109791096+ebaa17@users.noreply.github.com>
2024-04-03 02:51:57 +0200

    Update selectionSort.cpp

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -1,13 +1,6 @@
 #include <bits/stdc++.h>
 
-#define fast ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
-#define el '\n'
-#define ll  long long
-#define ld  long double
-#define all(x) x.begin(), x.end()
-#define yes "YES\n"
-#define no "NO\n"
-//#define test int test;cin>>test;while(test--)
+
 using namespace std;
 
 void selectionSort(int a[], int n) {
@@ -20,7 +13,6 @@ void selectionSort(int a[], int n) {
 }
 
 int main() {
-    fast
     int n;
     cin >> n;
     int a[n];
