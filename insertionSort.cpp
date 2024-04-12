b33a93c876f2c62a4ee98ffb0b16dff1589751b6
ebaa17 <109791096+ebaa17@users.noreply.github.com>
2024-04-12 22:30:26 +0200

    Update insertionSort.cpp

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
+#define ll long long
 
 
 using namespace std;
 
 
-void insertionSort(int arr[], int n) {
+void insertionSort(ll arr[], int n) {
     for (int i = 0, j; i < n; ++i) {
         int tmp = arr[i];
         for (j = i; j > 0 && tmp < arr[j - 1]; --j)
@@ -17,7 +18,7 @@ void insertionSort(int arr[], int n) {
 int main() {
     int n;
     cin >> n;
-    int a[n];
+    ll a[n];
     for (int i = 0; i < n; ++i) {
         cin >> a[i];
     }
