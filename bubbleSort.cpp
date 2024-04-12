726e6238ba6c5e1d9d86896a259e93a3d55b1a5b
ebaa17 <109791096+ebaa17@users.noreply.github.com>
2024-04-12 22:53:35 +0200

    Update bubbleSort.cpp

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,1 +1,36 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+
+using namespace std;
+
+void bubbleSort(ll arr[], int n){
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n-1; ++j) {
+            if(arr[j]>arr[j+1])
+                swap(arr[j],arr[j+1]);
+        }
+    }
+}
+
+int main() {
+    cout<<"Array size: ";
+    int n;
+    cin >> n;
+    ll arr[n];
+    cout << "Original array: ";
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+
+    cout << "Sorted array: ";
+
+    bubbleSort(arr, n);
+
+    for (int i = 0; i < n; ++i) cout << arr[i] << ' ';
+    cout << '\n';
+
+    return 0;
+}
+
 
